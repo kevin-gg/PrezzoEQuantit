9d412378531d924fe23bc6611e483d2cc57a4622
kevin trentini <trentinikevin6@gmail.com>
2021-03-14 14:37:14 +0100

    Add project files.

---
+++ PrezzoQuantitàWPF/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PrezzoQuantitàWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btmMostra_Click(object sender, RoutedEventArgs e)
+        {
+            int s;
+            if (txtSconto.Text != "")
+                s = int.Parse(txtSconto.Text);
+            else
+                s = 0;
+
+            if (s < 0 ||  s > 100)
+                {
+                MessageBox.Show("Sconto non accettato", "Attenzione", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+                else
+            {
+                if (txtQuantita.Text != "" && txtPrezzo.Text != "")
+                {
+
+
+                    try
+                    {
+                        double p = double.Parse(txtPrezzo.Text);
+                        int q = int.Parse(txtQuantita.Text);
+                        double prezzo = p * q;
+                        if (txtSconto.Text == "" ||  s < 20)
+                            {
+                            lblRisultato.Content = prezzo;
+                        }
+                            else
+                        {
+                            double sconto = (prezzo * s) / 100;
+                            double totale = prezzo - sconto;
+                            lblRisultato.Content = totale;
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        lblRisultato.Content = ex.Message;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Inserire tutti i valori richiesti", "Attenzione", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+        }
+    }
+}
